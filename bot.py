9852728f442438174211c377760ce6e86b2cd872
Urqkel <juzzyurquhart@gmail.com>
2025-10-05 20:31:41 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ def add_logo_to_card(card_image, logo_path, scale=0.18, padding=25):
     logo_ratio = logo.width / logo.height
     logo_width = int(card_width * scale)
     logo_height = int(logo_width / logo_ratio)
-    logo = logo.resize((logo_width, logo_height), Image.ANTIALIAS)
+    logo = logo.resize((logo_width, logo_height), Image.Resampling.LANCZOS)
     x_pos = (card_width - logo_width) // 2
     y_pos = card_height - logo_height - padding
     card.paste(logo, (x_pos, y_pos), logo)
